9f7b18d39e9cf2f68e9d8bb45cef3183f655f685
Adewale Alao <alaoadewale.n@gmail.com>
2022-04-12 11:40:02 +0100

    A program to print _putchar without using printf or puts

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* main - prints is posetive, negative
+* or zero
+* Return: returns zero if program exits well.
+*/
+int main(void)
+{
+	write(1, "_putchar\n", 13);
+	return (0);
+}
